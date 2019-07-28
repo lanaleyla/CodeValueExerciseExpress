896723b03cefb3ea1db297e34b3281460e2ff744
lanaleyla <lanali999@gmail.com>
2019-07-28 23:17:58 +0300

    exercise express finished

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -2,36 +2,34 @@ import { Request, Response, NextFunction, Router } from 'express';
 import { Category, Product } from '../models';
 import uuidv1 from 'uuid/v1';
 import categories from "../categories.json";
-import {productArray} from "../routes/products";
+import { productArray } from "../routes/products";
 
-const categoriesArray: Category[]=categories.Category; //list of categories
+const categoriesArray: Category[] = categories.Category; //list of categories
 
 //assign uniqe id's to the products list 
-for (let i=0;i<categoriesArray.length;i++)
-{
-    categoriesArray[i].id=uuidv1();
+for (let i = 0; i < categoriesArray.length; i++) {
+    categoriesArray[i].id = uuidv1();
 }
 
 const router = Router(); //our router
 
 //Requests:get,post,put,delete
-  
+
 //return all categories  
 router.get('/categories', (req, res, next) => {
     res.send(categoriesArray);
 });
 
 //return specific category
-router.get("/categories/:id",(req,res,next)=>{
+router.get("/categories/:id", (req, res, next) => {
     const id = req.params.id;
-    if(id.length>36) //check id lenght>36
+    if (id.length > 36) //check id lenght>36
     {
-      res.sendStatus(400);
-      return;
+        res.sendStatus(400);
+        return;
     }
     const matching = categoriesArray.find(o => o.id === id); //id not found
-    if (!matching)
-    {
+    if (!matching) {
         res.sendStatus(404);
         return;
     }
@@ -39,22 +37,21 @@ router.get("/categories/:id",(req,res,next)=>{
 });
 
 //return products list of a specific category
-router.get("/categories/:id/products",(req,res,next)=>{
+router.get("/categories/:id/products", (req, res, next) => {
     const id = req.params.id;
-    let productsAr:Product[]=[];
-    if(id.length>36) //check id lenght>36
+    let productsAr: Product[] = [];
+    if (id.length > 36) //check id lenght>36
     {
-      res.sendStatus(400);
-      return;
+        res.sendStatus(400);
+        return;
     }
     const matching = categoriesArray.find(o => o.id === id); //id not found
     if (!matching) {
         res.sendStatus(404);
         return;
     }
-    productsAr=findAllProducts(matching.name); //search for products
-    if(productsAr.length<1)
-    {
+    productsAr = findAllProducts(matching.name); //search for products
+    if (productsAr.length < 1) {
         res.sendStatus(404);
         return;
     }
@@ -63,46 +60,41 @@ router.get("/categories/:id/products",(req,res,next)=>{
 
 //add category to categories list
 router.post('/categories/:name', (req, res) => {
-    const category: Category=req.body;
+    const category: Category = req.body;
     category.id = uuidv1();         //assign id
-    category.name=req.params.name; 
+    category.name = req.params.name;
     categoriesArray.push(category); //assign new category
     res.status(201).send(category);
 });
 
 //update category by given id
-router.put('/categories/:id/:name',findProjectIndex,(req, res) => {
+router.put('/categories/:id/:name', findProjectIndex, (req, res) => {
     const category: Category = req.body;
     const id = req.params.id;
     const { categoryId, matchingIndex } = res.locals;
-    const matching = categoriesArray.find(o => o.id === id); //search id
-    if (!matching) {
-        res.sendStatus(404); //id not found
-        return;
-    }
-    if(id.length>36) //check id lenght>36
+    if (id.length > 36) //check id lenght>36
     {
         res.sendStatus(400);
         return;
     }
-    category.id=id;
+    category.id = id;
     category.name = req.params.name;
-    categoriesArray[matchingIndex]=category; //assign updated category
+    categoriesArray[matchingIndex] = category; //assign updated category
     res.send(category);
-    },
+},
 );
 
 //delete category by id
-router.delete('/categories/:id',findProjectIndex,(req, res) => {
+router.delete('/categories/:id', findProjectIndex, (req, res) => {
     const id = req.params.id;
-    if(id.length>36) //check id lenght>36
+    if (id.length > 36) //check id lenght>36
     {
         res.sendStatus(400);
         return;
     }
     categoriesArray.splice(res.locals.matchingIndex, 1);
     res.sendStatus(204);
-    },
+},
 );
 
 //find the index of the entered id
@@ -111,8 +103,8 @@ function findProjectIndex(req: Request, res: Response, next: NextFunction) {
     const matchingIndex = categoriesArray.findIndex(o => o.id === id);
 
     if (matchingIndex < 0) {
-    res.sendStatus(404);
-    return;
+        res.sendStatus(404);
+        return;
     }
     res.locals.matchingIndex = matchingIndex;
     res.locals.projectId = id;
@@ -120,13 +112,11 @@ function findProjectIndex(req: Request, res: Response, next: NextFunction) {
 }
 
 //find products with the given category name, return products list
-function findAllProducts(name:string):Product[]{
-    const product:Product[]=[];
+function findAllProducts(name: string): Product[] {
+    const product: Product[] = [];
 
-    for(let i=0;i<productArray.length;i++)
-    {
-        if(productArray[i].categoryId===name)
-        {
+    for (let i = 0; i < productArray.length; i++) {
+        if (productArray[i].categoryId === name) {
             product.push(productArray[i]);
         }
     }
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,34 +3,32 @@ import { Product } from '../models';
 import uuidv1 from 'uuid/v1';
 import products from "../products.json";
 
-export const productArray: Product[]=products.Product; //list of products
+export const productArray: Product[] = products.Product; //list of products
 
 //assign uniqe id's to the products list 
-for (let i=0;i<productArray.length;i++)
-{
-  productArray[i].id=uuidv1();
+for (let i = 0; i < productArray.length; i++) {
+  productArray[i].id = uuidv1();
 }
 
 const router = Router(); //our router
 
 //Requests:get,post,put,delete
-  
+
 //return all products  
-router.get('/products',(req, res, next) => {
-    res.send(productArray);
+router.get('/products', (req, res, next) => {
+  res.send(productArray);
 });
 
 //return specific product
-router.get("/products/:id",(request,response,next)=>{
+router.get("/products/:id", (request, response, next) => {
   const id = request.params.id;
-  if(id.length>36) //check id lenght>36
+  if (id.length > 36) //check id lenght>36
   {
     response.sendStatus(400);
     return;
   }
   const matching = productArray.find(o => o.id === id); //id not found
-  if (!matching)
-  {
+  if (!matching) {
     response.sendStatus(404);
     return;
   }
@@ -39,60 +37,55 @@ router.get("/products/:id",(request,response,next)=>{
 
 //add product to products list
 router.post('/products/:name-:categoryId-:itemsInStock', (req, res) => {
-  const product: Product=req.body;
+  const product: Product = req.body;
   product.id = uuidv1();      //assign id
-  if(req.params.name.length<3) //if products name length is less then 3 charachters 
+  if (req.params.name.length < 3) //if products name length is less then 3 charachters 
   {
     res.sendStatus(409);
     return;
   }
-  product.categoryId=req.params.categoryId; //assign new product
-  product.name=req.params.name;
-  product.itemsInStock=req.params.itemsInStock;
+  product.categoryId = req.params.categoryId; //assign new product
+  product.name = req.params.name;
+  product.itemsInStock = req.params.itemsInStock;
   productArray.push(product);
   res.status(201).send(product);
 });
 
 //update product
-router.put('/products/:id/:name-:categoryId-:itemsInStock',findProjectIndex,(req, res) => {
+router.put('/products/:id/:name-:categoryId-:itemsInStock', findProjectIndex, (req, res) => {
   const product: Product = req.body;
   const id = req.params.id;
-  const { productId, matchingIndex } = res.locals;
-  const matching = productArray.find(o => o.id === id); //search id
-  if (!matching) {
-    res.sendStatus(404); //id not found
-    return;
-  }
-  if(id.length>36) //check id lenght>=36
+  const { matchingIndex } = res.locals;
+  if (id.length > 36) //check id lenght>=36
   {
     res.sendStatus(400);
     return;
   }
-  if(req.params.name.length<3) //if products name length is less then 3 charachters 
+  if (req.params.name.length < 3) //if products name length is less then 3 charachters 
   {
     res.sendStatus(409);
     return;
   }
-  product.id=id; //assign updated product
+  product.id = id; //assign updated product
   product.name = req.params.name
   product.categoryId = req.params.categoryId;
   product.itemsInStock = req.params.itemsInStock;
-  productArray[matchingIndex]=product;
+  productArray[matchingIndex] = product;
   res.send(product);
-  },
+},
 );
 
 //delete product by id
-router.delete('/products/:id',findProjectIndex,(req, res) => {
+router.delete('/products/:id', findProjectIndex, (req, res) => {
   const id = req.params.id;
-  if(id.length>36) //check id lenght>36
+  if (id.length > 36) //check id lenght>36
   {
     res.sendStatus(400);
     return;
   }
   productArray.splice(res.locals.matchingIndex, 1);
   res.sendStatus(204);
-  },
+},
 );
 
 //find the index in products array of the entered product id
@@ -105,7 +98,6 @@ function findProjectIndex(req: Request, res: Response, next: NextFunction) {
     return;
   }
   res.locals.matchingIndex = matchingIndex; //save index and id and return them
-  res.locals.productId = id;
   next();
 }
 
